094c8359432275b2232313ff334418c0e2500e57
Dae Choi <dchoi@domanic.io>
2021-02-03 06:13:15 -0500

    Adds form component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import './App.css'
 import ApplicationSetup from './features/ApplicationSetup'
+import FormTest from './components/formtest'
 
 function App() {
+  const onNewColor = (title: string, color: string) => {
+    console.log(`title ${title} and color: ${color}`)
+  }
   return (
     <div className="App">
       <ApplicationSetup>
         <h2>This is the prop to be sent</h2>
+        <FormTest onNewColor={onNewColor} />
       </ApplicationSetup>
     </div>
   )
