5884f82da36e144719ebda3992f35f120702e945
Dae Choi <dchoi@domanic.io>
2021-02-13 11:02:41 -0500

    WIP brainstorming features - Dabble at the user profile - Updated the doc to capture thoughts/ideas

--- frontend/src/components/App.tsx
+++ frontend/src/components/App.tsx
@@ -8,7 +8,13 @@ const App: React.FC = () => {
     <BrowserRouter>
       <Navbar />
       <div>
-        <h1>Domanic</h1>
+        <h1>Number my days!</h1>
+        <h2>A Scheduling software by hour by subject</h2>
+        <p>A thousand miles journey begins with one step</p>
+        <p>that begins with a character....that begins with one letter...</p>
+        would be awesome to have vim binding so that I can do everything on keyboard without leaving it...at
+        the speed of thought. and be able to open anywhere...including from the phone...whatever i was
+        thinking or want to contribute.
         <Loading isLoading={false} />
       </div>
     </BrowserRouter>
